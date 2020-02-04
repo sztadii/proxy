cdc6b3ea8f940b0ade207ab77c93386acc549caf
Krystian Sztadhaus <sztadii@gmail.com>
2020-02-04 15:20:30 +0400

    Create form to save encoded cookie

--- src/modules/proxy/proxy.controller.ts
+++ src/modules/proxy/proxy.controller.ts
@@ -1,8 +1,13 @@
 import * as express from 'express'
 import * as requestProxy from 'express-request-proxy'
+import * as path from 'path'
 import { decodeString, encodeString } from '../../helpers/security-helpers'
 
 const router = express.Router()
+
+router.get('/template', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/proxy.template.html'))
+})
 
 router.get('*', (req, res, next) => {
   const { url } = req.query
