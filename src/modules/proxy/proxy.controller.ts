ab71db36de6c581daddbc80abb9cdec2585f6a13
Krystian Sztadhaus <sztadii@gmail.com>
2020-12-17 23:26:43 +0400

    Split router paths and handlers

--- src/modules/proxy/proxy.controller.ts
+++ src/modules/proxy/proxy.controller.ts
@@ -1,15 +1,13 @@
-import express from 'express'
+import { Request, Response, NextFunction } from 'express'
 import requestProxy from 'express-request-proxy'
 import path from 'path'
 import { decodeBase64 } from '../../helpers/security-helpers'
 
-const router = express.Router()
-
-router.get('/template', (req, res, next) => {
+export function renderFormTemplate(req: Request, res: Response) {
   res.sendFile(path.join(__dirname + '/proxy.template.html'))
-})
+}
 
-router.get('*', (req, res, next) => {
+export function proxyHandler(req: Request, res: Response, next: NextFunction) {
   const cookieKey = process.env.URL_COOKIE_KEY
   const urlFromEnv = process.env.PROXY_URL
   const encodedUrlFromCookie = req.cookies[cookieKey]
@@ -23,13 +21,11 @@ router.get('*', (req, res, next) => {
   const urlFromCookie = decodeBase64(encodedUrlFromCookie)
   const requestedPath = urlFromEnv ? `${urlFromEnv}/*` : `${urlFromCookie}/*`
 
-  // Please keep it, useful during errors
-  console.log('urlFromEnv', urlFromEnv)
-  console.log('urlFromCookie', urlFromCookie)
-  console.log('requestedPath', requestedPath)
+  // Please keep it, useful during debugging
+  console.log({ urlFromEnv })
+  console.log({ urlFromCookie })
+  console.log({ requestedPath })
   console.log('---')
 
   return requestProxy({ url: requestedPath })(req, res, next)
-})
-
-export default router
+}
